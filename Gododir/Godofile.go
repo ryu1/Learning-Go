1f0b6928141d3a44cae7692a1fcec89147cae334
石塚隆一 <jeremy.hillary.boob@gmail.com>
2015-07-07 20:08:36 +0900

    Fix golint Task in Godofile

--- Gododir/Godofile.go
+++ Gododir/Godofile.go
@@ -8,15 +8,25 @@ import (
 func tasks(p *Project) {
 	Env = `GOPATH=.vendor::$GOPATH`
 
-	p.Task("buildAll", D{"golint"}, func() error {
+//	p.Task("install", func() {
+//		Run("go get github.com/golang/lint/golint")
+//		Run("go get github.com/mgutz/goa")
+//		Run("go get github.com/robertkrimen/godocdown/godocdown")
+//	})
+
+	p.Task("buildAll", D{"lint"}, func() error {
 		return Bash(`gom exec gox ./src/...`)
 	})
 
-	p.Task("golint", func() error {
-		return Bash(`gom exec golint ./src/...`)
+	p.Task("lint", func() error {
+		//Bash(`gom exec golint ./src/...`)
+		Run("golint .")
+		Run("gofmt -w -s ./src/...")
+		Run("go vet ./src/...")
+		return
 	}).Watch("**/*.go")
 
-	p.Task("testAll", D{"golint"}, func() error {
+	p.Task("testAll", D{"lint"}, func() error {
 		return Bash(`gom exec go test -v ./src/...`)
 	}).Watch("**/*.go")
 }
