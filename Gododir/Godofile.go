4c789401d9e6117a51bff02e8a39979054ba2d7a
石塚隆一 <jeremy.hillary.boob@gmail.com>
2015-07-07 19:29:40 +0900

    Add buildAll, golint, testAll Task in Godofile

--- Gododir/Godofile.go
+++ Gododir/Godofile.go
@@ -8,9 +8,17 @@ import (
 func tasks(p *Project) {
 	Env = `GOPATH=.vendor::$GOPATH`
 
-	p.Task("buildAll", func() error {
-		return Bash(`gom exec gox ./src/... `)
+	p.Task("buildAll", D{"golint"}, func() error {
+		return Bash(`gom exec gox ./src/...`)
 	})
+
+	p.Task("golint", func() error {
+		return Bash(`gom exec golint ./src/...`)
+	}).Watch("**/*.go")
+
+	p.Task("testAll", D{"golint"}, func() error {
+		return Bash(`gom exec go test -v ./src/...`)
+	}).Watch("**/*.go")
 }
 
 func main() {
